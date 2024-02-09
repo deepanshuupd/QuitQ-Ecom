6b53bf81c7060ed69140a01834782e43759aee5f
deepanshuupd <upadhayaydeepanshu8@gmail.com>
2024-02-09 08:57:03 +0530

    added table name

--- src/main/java/com/hexaware/QuitQ/entity/Order.java
+++ src/main/java/com/hexaware/QuitQ/entity/Order.java
@@ -8,8 +8,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="order")
 public class Order {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
